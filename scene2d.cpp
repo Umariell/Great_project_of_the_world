495fdc5e1c8a110601eddbc06d696397a3929142
Umariell <Endraum-7>
2018-05-27 18:46:03 +0300

    eee

--- scene2d.cpp
+++ scene2d.cpp
@@ -22,7 +22,7 @@ void Scene2D::wheelEvent(QWheelEvent* event)
 {
     qreal scaleFactor = qPow(2.0, event->delta() / 240.0);          //How fast we zoom
     qreal factor = transform().scale(scaleFactor, scaleFactor).mapRect(QRectF(0, 0, 1, 1)).width();
-    if(0.05 < factor && factor < 10)                                //Zoom factor limitation
+    if(0.05 < factor && factor < 1000000)                                //Zoom factor limitation
     scale(scaleFactor, scaleFactor);
 }
 
